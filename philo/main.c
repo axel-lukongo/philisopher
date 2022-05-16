7944202a82a270d9426fbe5c7d689703e34d445e
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-05-16 13:37:53 +0200

    middle

--- philo/main.c
+++ philo/main.c
@@ -6,26 +6,27 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:48:51 by alukongo          #+#    #+#             */
-/*   Updated: 2022/05/12 20:26:42 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/05/13 17:58:39 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
+//pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 void	*func1(void * arg)
 {
-	pthread_mutex_lock(&mutex);
+	t_data data;
 	(void) arg;
 	int i = 0;
+	pthread_mutex_init(&data.write, NULL);
+	pthread_mutex_lock(&data.write);
 	while (i < 10)
 	{
 		usleep(100000);
 		printf("i = %d\n", i++);
 	}
 	printf("next thread\n");
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&data.write);
 	return (NULL);
 }
 
@@ -42,6 +43,8 @@ void	set_philo(int nb_philo, int ac, char **av)
 	int i;
 
 	init_data(&data, av, ac);
+//	t_data data;
+	pthread_mutex_init(&data.write, NULL);
 	i = 0;
 	while (i < nb_philo)
 	{
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:36:58 by alukongo          #+#    #+#             */
-/*   Updated: 2022/05/12 16:26:08 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:44:29 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ typedef struct s_data
 	int time_to_eat;
 	int time_to_sleep;
 	int time_eat_each;
+	pthread_mutex_t write;
+	pthread_mutex_t lock;
 } t_data;
 
 int	parsing(int ac, char **av);
